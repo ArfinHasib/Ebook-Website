014b9383ed05ca7804fe7f439dce02af30a8166d
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-23 23:51:11 +0600

    Adding shadow header when scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,6 +41,14 @@ if (loginClose) {
 }
 
 /*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () => {
+   const header = document.getElementById('header');
+   // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+   this.scrollY >= 50
+      ? header.classList.add('shadow-header')
+      : header.classList.remove('shadow-header');
+};
+window.addEventListener('scroll', shadowHeader);
 
 /*=============== HOME SWIPER ===============*/
 let swiperHome = new Swiper('.home__swiper', {
